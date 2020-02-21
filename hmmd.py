b7034f62b41b0edbfde0b001b5c6dd75b9343f36
isikriza <rizaisikk@gmail.com>
2020-02-22 02:19:56 +0300

    Fixed some bugs.

--- hmmd.py
+++ hmmd.py
@@ -9,23 +9,30 @@ def random_normalized(d1, d2):
 
 class HMM:
     def __init__(self, M):
-        self.M = M
+        self.M = M  # number of hidden states
 
-    def fit(self, X, max_iter=30, seed=42):
-        np.random.seed(seed)
+    def fit(self, X, max_iter=30):
+        np.random.seed(123)
+        # train the HMM model using the Baum-Welch algorithm
+        # a specific instance of the expectation-maximization algorithm
 
+        # determine V, the vocabulary size
+        # assume observables are already integers from 0..V-1
+        # X is a jagged array of observed sequences
         V = max(max(x) for x in X) + 1
         N = len(X)
 
-        self.pi = np.ones(self.M) / self.M
-        self.A = random_normalized(self.M, self.M)
-        self.B = random_normalized(self.M, V)
+        self.pi = np.ones(self.M) / self.M  # initial state distribution
+        self.A = random_normalized(self.M, self.M)  # state transition matrix
+        self.B = random_normalized(self.M, V)  # output distribution
+
+        print("initial A:", self.A)
+        print("initial B:", self.B)
 
         costs = []
         for it in range(max_iter):
-            if it % 10 == 0: # print every 10 epochs
+            if it % 10 == 0:
                 print("it:", it)
-
             alphas = []
             betas = []
             P = np.zeros(N)
@@ -35,20 +42,20 @@ class HMM:
                 alpha = np.zeros((T, self.M))
                 alpha[0] = self.pi * self.B[:, x[0]]
                 for t in range(1, T):
-                    alpha[t] = alpha[t-1].dot(self.A) * self.B[:, x[t]]
+                    alpha[t] = alpha[t - 1].dot(self.A) * self.B[:, x[t]]
                 P[n] = alpha[-1].sum()
                 alphas.append(alpha)
 
                 beta = np.zeros((T, self.M))
                 beta[-1] = 1
-                for t in range(T-2, -1, -1):
-                    beta[t] = self.A.dot(self.B[:, x[t+1]]) * beta[t+1]
+                for t in range(T - 2, -1, -1):
+                    beta[t] = self.A.dot(self.B[:, x[t + 1]] * beta[t + 1])
                 betas.append(beta)
 
             cost = np.sum(np.log(P))
             costs.append(cost)
 
-            self.pi = np.sum((alphas[n][0] * betas[n][0])/P[n] for n in range(N)) / N
+            self.pi = np.sum((alphas[n][0] * betas[n][0]) / P[n] for n in range(N)) / N
 
             den1 = np.zeros((self.M, 1))
             den2 = np.zeros((self.M, 1))
@@ -57,30 +64,28 @@ class HMM:
             for n in range(N):
                 x = X[n]
                 T = len(x)
-
                 den1 += (alphas[n][:-1] * betas[n][:-1]).sum(axis=0, keepdims=True).T / P[n]
                 den2 += (alphas[n] * betas[n]).sum(axis=0, keepdims=True).T / P[n]
 
+                # numerator for A
                 a_num_n = np.zeros((self.M, self.M))
                 for i in range(self.M):
                     for j in range(self.M):
-                        for t in range(T-1):
-                            a_num_n[i, j] += alphas[n][t, i] * self.A[i, j] * self.B[j, x[t+1]] * betas[n][t+1, j]
+                        for t in range(T - 1):
+                            a_num_n[i, j] += alphas[n][t, i] * self.A[i, j] * self.B[j, x[t + 1]] * betas[n][t + 1, j]
                 a_num += a_num_n / P[n]
 
+                # numerator for B
                 b_num_n = np.zeros((self.M, V))
                 for i in range(self.M):
-                    for j in range(V):
-                        for t in range(T):
-                            if x[t] == j:
-                                b_num_n[i, j] += alphas[n][t, i] * betas[n][t, i]
+                    for t in range(T):
+                        b_num_n[i, x[t]] += alphas[n][t, i] * betas[n][t, i]
                 b_num += b_num_n / P[n]
             self.A = a_num / den1
             self.B = b_num / den2
-
         print("A:", self.A)
         print("B:", self.B)
-        print("pi", self.pi)
+        print("pi:", self.pi)
 
         plt.plot(costs)
         plt.show()
@@ -99,15 +104,17 @@ class HMM:
     def log_likelihood_multi(self, X):
         return np.log(self.likelihood_multi(X))
 
-    def get_state_sequence(self, x):    # Viterbi
+    def get_state_sequence(self, x):
+        # returns the most likely state sequence given observed sequence x
+        # using the Viterbi algorithm
         T = len(x)
         delta = np.zeros((T, self.M))
         psi = np.zeros((T, self.M))
-        delta[0] = self.pi * self.B[: x[0]]
+        delta[0] = self.pi*self.B[:,x[0]]
         for t in range(1, T):
             for j in range(self.M):
-                delta[t, j] = np.max(delta[t-1] * self.A[:, j]) * self.B[j, x[t]]
-                psi[t, j] = np.argmax(delta[t-1] * self.A[:, j])
+                delta[t,j] = np.max(delta[t-1]*self.A[:,j]) * self.B[j, x[t]]
+                psi[t,j] = np.argmax(delta[t-1]*self.A[:,j])
 
         # backtrack
         states = np.zeros(T, dtype=np.int32)
