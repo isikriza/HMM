682ab86baa26e143d9577fb3848aa9979c0cfd79
isikriza <rizaisikk@gmail.com>
2020-02-22 00:22:19 +0300

    HMM class added.

---
+++ generate_ht.py
@@ -0,0 +1,33 @@
+from __future__ import print_function, division
+from builtins import range
+import numpy as np
+
+symbol_map = ['H', 'T']
+pi = np.array([0.5, 0.5])
+A = np.array([[0.1, 0.9], [0.8, 0.2]])
+B = np.array([[0.6, 0.4], [0.3, 0.7]])
+M, V = B.shape
+
+
+def generate_sequence(N):
+    s = np.random.choice(range(M), p=pi) # initial state
+    x = np.random.choice(range(V), p=B[s]) # initial observation
+    sequence = [x]
+    for n in range(N-1):
+        s = np.random.choice(range(M), p=A[s]) # next state
+        x = np.random.choice(range(V), p=B[s]) # next observation
+        sequence.append(x)
+    return sequence
+
+
+def main():
+    with open('coin_data.txt', 'w') as f:
+        for n in range(50):
+            sequence = generate_sequence(30)
+            sequence = ''.join(symbol_map[s] for s in sequence)
+            print(sequence)
+            f.write("%s\n" % sequence)
+
+
+if __name__ == '__main__':
+    main()
